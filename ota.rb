f2da4eb24d07433529ac577746d4c12912af744b
Ryan D Johnson <ryandjohnson@gmail.com>
2011-04-29 16:34:21 -0700

    Initial commit

---
+++ ota.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'erubis'
+require 'yaml'
+
+def path_for( file )
+  File.join( File.dirname( __FILE__ ), file )
+end
+
+File.unlink( *Dir[ path_for('*.plist') ] )
+
+config = YAML.load( File.read( path_for( 'ota.yml' ) ) )
+plist_template = Erubis::EscapedEruby.new( File.read( path_for( 'ota.plist.erb' ) ) )
+index_template = Erubis::EscapedEruby.new( File.read( path_for( 'ota.html.erb' ) ) )
+
+apps = []
+
+Dir[path_for('*.ipa')].each do |ipa_file|
+  ipa_file = File.basename( ipa_file )
+  match = /^(.+?)[.]app[.]AdHoc[.](.+?)[.]ipa$/i.match( ipa_file )
+  raise "Can't parse #{ipa_file}" unless match
+
+  apps << ( app = { 'name' => match[1], 'version' => match[2] } )
+  app.merge! config[app['name']]
+end
+
+apps.each do |app|
+  File.open( path_for( "#{app['name']}.app.AdHoc.#{app['version']}.plist" ), 'w' ) do |f|
+    f.print plist_template.result( :config => config, :app => app )
+  end
+end
+
+File.open( path_for( 'index.html' ), 'w' ) do |f|
+  f.print index_template.result( :config => config, :apps => apps )
+end
